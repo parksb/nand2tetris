7334ff4dabbeb6051d8975fd89f584a1c27e22f9
parksb <parkgds@gmail.com>
2023-03-19 00:30:24 +0900

    Finish the project 4-mult

--- 4-machine-language/mult/Mult.asm
+++ 4-machine-language/mult/Mult.asm
@@ -9,4 +9,28 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+@R2
+M=0
+@R0
+D=M
+@END
+D;JEQ
+@R1
+D=M
+@END
+D;JEQ
+(LOOP)
+    @R0
+    D=M
+    @R2
+    M=D+M
+    @R1
+    M=M-1
+    D=M
+    @END
+    D;JEQ
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
